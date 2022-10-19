459102719bc7d58e07ae98846df6bd36cfc74d9d
Kegen Guyll <kegen.guyll@acretrader.com>
2022-10-19 14:16:25 -0500

    created module name wrapper for d-3

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,8 @@ module.exports = {
   collectCoverage: true,
   clearMocks: true,
   coverageDirectory: 'coverage',
+  moduleNameMapper: {
+    'd3-array': '<rootDir>/node_modules/d3-array/dist/d3-array.min.js',
+    'd3-polygon': '<rootDir>/node_modules/d3-polygon/dist/d3-polygon.min.js',
+  },
 };
